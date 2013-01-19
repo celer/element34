ea736fc79521c95f1d8c4da5347947f0d7c5f5d6
celer <celer@husky.(none)>
2013-01-18 22:18:53 -0700

    Updated install

--- install.js
+++ install.js
@@ -3,17 +3,32 @@ var path = require('path');
 var fs = require('fs');
 var cp = require('child_process');
 
-cp.exec("mkdir ext").on("close",function(){
 
-	console.log("Downloading selenium (this could take a while)");
-	request("http://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar").pipe(fs.createWriteStream(path.join(__dirname,"ext/selenium-server-standalone.jar"))).on("close",function(){
-
+var compile=function(){
 		console.log("Compiling java");
 		var proc = cp.exec("(cd lib/java && javac -classpath ../../ext/selenium-server-standalone.jar *.java)");
 
 		proc.stdout.on("data",console.log);
 		proc.stderr.on("data",console.log);
 
-	});
+}
 
-});
+
+
+
+
+var jarOut=path.join(__dirname,"ext/selenium-server-standalone.jar");
+if(!fs.existsSync(jarOut)){
+
+	cp.exec("mkdir ext").on("close",function(){
+
+		console.log("Downloading selenium (this could take a while)");
+		request("http://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar").pipe(fs.createWriteStream(jarOut)).on("close",function(){
+
+			compile();
+		});
+
+	});
+} else {
+	compile();
+}
