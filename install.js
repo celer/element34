e58faace59d0a51537501a51d2805430ca153576
celer <celer@coyote.local>
2013-01-18 19:40:07 -0700

    Fixed issue with installation script

--- install.js
+++ install.js
@@ -3,14 +3,17 @@ var path = require('path');
 var fs = require('fs');
 var cp = require('child_process');
 
+cp.exec("mkdir ext").on("close",function(){
 
-console.log("Downloading selenium (this could take a while)");
-request("http://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar").pipe(fs.createWriteStream(path.join(__dirname,"ext/selenium-server-standalone.jar"))).on("close",function(){
+	console.log("Downloading selenium (this could take a while)");
+	request("http://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar").pipe(fs.createWriteStream(path.join(__dirname,"ext/selenium-server-standalone.jar"))).on("close",function(){
 
-	console.log("Compiling java");
-	var proc = cp.exec("(cd lib/java && javac -classpath ../../ext/selenium-server-standalone.jar *.java)");
+		console.log("Compiling java");
+		var proc = cp.exec("(cd lib/java && javac -classpath ../../ext/selenium-server-standalone.jar *.java)");
 
-	proc.stdout.on("data",console.log);
-	proc.stderr.on("data",console.log);
+		proc.stdout.on("data",console.log);
+		proc.stderr.on("data",console.log);
+
+	});
 
 });
